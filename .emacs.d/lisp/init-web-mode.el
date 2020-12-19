ed8738c0c32d7edd4936da86acfdfaf075e29d24
endenwer <endenwer@gmail.com>
2020-12-19 13:15:03 +0300

    Add emmet and change web mode settings

--- .emacs.d/lisp/init-web-mode.el
+++ .emacs.d/lisp/init-web-mode.el
@@ -2,10 +2,19 @@
 
 (use-package web-mode
   :mode
-  ("\\.tsx\\'")
+  ("\\.tsx\\'" "\\.svelte\\'")
   :custom
+  (web-mode-enable-auto-quoting nil)
+  (web-mode-block-padding 2)
+  (web-mode-style-padding 2)
+  (web-mode-script-padding 2)
   (web-mode-markup-indent-offset 2)
   (web-mode-css-indent-offset 2)
   (web-mode-code-indent-offset 2))
+
+(use-package emmet-mode
+  :hook (web-mode)
+  :custom
+  (emmet-expand-jsx-className? t))
 
 (provide 'init-web-mode)
