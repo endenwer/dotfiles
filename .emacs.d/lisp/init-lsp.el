c74bdbe86439347843b1ea0f5cd753c922974cc4
endenwer <endenwer@gmail.com>
2021-07-04 13:57:21 +0300

    Disable breadcrumb for lsp

--- .emacs.d/lisp/init-lsp.el
+++ .emacs.d/lisp/init-lsp.el
@@ -7,7 +7,10 @@
    (js-mode . lsp-deferred)
    (ruby-mode . lsp-deferred)
    (lsp-mode . lsp-enable-which-key-integration))
-  :commands (lsp lsp-deferred)
+  :custom
+  (lsp-headerline-breadcrumb-enable nil)
+  :commands
+  (lsp lsp-deferred)
   :init
   (setq gc-cons-threshold 100000000)
   (setq read-process-output-max (* 1024 1024)))
