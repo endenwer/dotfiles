b1e6e365de48b36cbafa709b65e6b90c5ec25bf7
endenwer <endenwer@gmail.com>
2021-08-11 11:09:02 +0300

    Clean fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,4 @@ set fish_greeting
 
 fish_default_key_bindings
 
-set -U fish_user_paths $HOME/bin $GOPATH/bin $HOME/.fzf/bin $HOME/.rbenv/bin $HOME/.rbenv/shims /usr/local/bin
+set -U fish_user_paths $HOME/bin $GOPATH/bin $HOME/.fzf/bin /usr/local/bin
