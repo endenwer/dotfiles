f05bf15d13717615b10c9594df42c077040174fc
endenwer <endenwer@gmail.com>
2024-02-06 16:14:22 +0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,11 @@
 alias e="emacsclient"
+alias python="python3"
 
 # remove greeting
 set fish_greeting
 
 fish_default_key_bindings
+
+set -x GOPATH /users/endenwer/go
 
 set -U fish_user_paths $HOME/bin $GOPATH/bin $HOME/.fzf/bin /usr/local/bin
