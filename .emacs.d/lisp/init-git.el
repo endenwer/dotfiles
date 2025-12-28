668df166569ce434e510fa603dada5704c59fc7f
endenwer <endenwer@gmail.com>
2025-12-28 15:17:10 +0700

    Change magit size for big screen

--- .emacs.d/lisp/init-git.el
+++ .emacs.d/lisp/init-git.el
@@ -5,7 +5,9 @@
   (:states 'normal
    :prefix leader-key
    "gg" 'magit-status
-   "gb" 'magit-blame-addition))
-
+   "gb" 'magit-blame-addition)
+  :custom
+  (split-width-threshold 200)
+  (split-height-threshold 120))
 
 (provide 'init-git)
